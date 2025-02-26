94a076f069890f62cca9b5498a198335af30bb34
up240161 <up240161@alumnos.upa.edu.mx>
2025-02-26 15:01:38 -0600

    new file:   01_DayExercises.py new file:   02_DayExercises.py new file:   03_DayExercises.py

---
+++ 01_DayExercises.py
@@ -0,0 +1,27 @@
+print(4 + 3)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 % 4)
+print(3 // 4)
+
+Name = 'Diego'
+Family_Name = 'Garduño González'
+country = 'México'
+python = 'python'
+
+print('nombre:', Name)
+print('Apellidos:', Family_Name)
+print('Pais:', country)
+print ('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type([Name, 'Python',country]))
+print(type({'Name': Name, 'Country': country}))
+print(type('Diego'))
+print(type('Garduño González'))
+print(type('México'))
---
+++ 02_DayExercises.py
@@ -0,0 +1,35 @@
+print("Hello, World!")
+print(len("Hello, World!"))
+print(type("Hello, World!"))
+print(str(10))
+print(int(10))
+print(float(10))
+
+
+first_name = (input("enter your name: "))
+last_name = "González"
+country = "Mexico"
+city = "Aguascalientes"
+age = (input("enter your age: "))
+is_married = False
+skills = ["HTML", "CSS", "JS", "Python"]
+person_info = {
+  "first_name": first_name,
+   "last_name": last_name,
+   "country": country,
+    "city": city,
+    "age": age,
+   "is_married": is_married,
+    "skills": skills
+}
+print("Hello,", first_name)
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Country:", country)
+print("City:", city)
+print("Age:", age)
+print("Is married:", is_married)
+print("Skills:", skills)
+print("Person info:", person_info)
+
+print(first_name, last_name, country, city, age, is_married)

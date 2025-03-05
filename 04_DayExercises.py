cfa7a8dc15ad1ebda8f1e9af3a1aa11972d83efd
up240161 <up240161@alumnos.upa.edu.mx>
2025-03-05 15:31:24 -0600

    new file:   04_DayExercises.py

---
+++ 04_DayExercises.py
@@ -0,0 +1,32 @@
+# Excersise 1
+print('thirty'+'' + 'days' + ' of ' + ' ' + 'python')
+
+#Excersise 2
+print('Coding'+''+'For'+' '+'All')
+
+#Excersise 3
+company='Coding For All'
+
+#Excersise 4
+print(company)
+
+#Excersise 5
+print(len(company))
+
+#Excersise 6
+
+print(company.upper())
+
+#Excersise 7
+
+print(company.lower())
+
+#Excersise 8
+
+print(company.capitalize())
+
+#Excersise 9
+
+print(company[0:6])
+
+#Excersise 10
